4347d7a51c15e5683475d5e6a6a29dc2cb686840
Thomas Leonard <talex5@gmail.com>
2019-12-30 17:55:24 +0000

    Disable components we don't need for CueKeeper
    Causes opam conflicts. Also, add missing ocplib-endian dependency so we
    work with newer cstruct versions.
    
    Cherry-picked from 9ed31332b7e956a091449eba4c6414f7f4f6aa6a.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -11,16 +11,7 @@ let opam = Pkg.opam_file ~lint_deps_excluding:None "opam"
 
 let () =
   Pkg.describe ~opams:[opam] "git" @@ fun c ->
-  let http = Conf.value c http in
-  let unix = Conf.value c unix in
-  let mirage = Conf.value c mirage in
-  let test = unix && http && mirage in
   Ok [
     Pkg.mllib ~api:["Git"] "lib/git.mllib";
     Pkg.mllib "lib/top/git-top.mllib";
-    Pkg.mllib ~cond:mirage "lib/mirage/git-mirage.mllib";
-    Pkg.mllib ~cond:http   "lib/http/git-http.mllib";
-    Pkg.mllib ~cond:unix   "lib/unix/git-unix.mllib";
-    Pkg.bin   ~cond:unix   "bin/ogit" ~dst:"ogit";
-    Pkg.test  ~cond:test ~dir:"lib_test" "lib_test/test" ~args:(Cmd.v "-q");
   ]
